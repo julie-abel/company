33e176c8cd0f97af6f19ddb55ae00ee5793b27a5
julie-abel <julie.abeleva@gmail.com>
2025-03-10 17:48:51 +0300

    commit2

--- company_users.py
+++ company_users.py
@@ -2,25 +2,36 @@ class User:
 
     def __init__(self, user_id: int, name: str, access_level: str = 'user'):
 
-        self.user_id = user_id  # Присваивание ID
-        self.name = name  # Присваивание имени пользователя
-        self.access_level = access_level  # Уровень доступа ('user' для обычных сотрудников)
+        self.__user_id = user_id  #   ID
+        self.__name = name  #   имя
+        self.__access_level = access_level  # уровень доступа
 
     def __str__(self):
 
-        return f"User(ID: {self.user_id}, Name: {self.name}, Access: {self.access_level})"
+        return f"User(ID: {self.__user_id}, Name: {self.__name}, Access: {self.__access_level})"
 
     def get_info(self):
+
         return {
-            'ID': self.user_id,
-            'Name': self.name,
-            'Access Level': self.access_level
+            'ID': self.__user_id,
+            'Name': self.__name,
+            'Access Level': self.__access_level
         }
 
+    def get_user_id(self):
+        return self.__user_id
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name: str):
+        self.__name = name
+
+    def get_access_level(self):
+        return self.__access_level
+
+
 class Admin(User):
-    """
-    наследуется от User.
-    """
 
     def __init__(self, user_id: int, name: str):
 
@@ -29,13 +40,13 @@ class Admin(User):
     def add_user(self, user_list: list, user: User):
 
         user_list.append(user)
-        print(f"Пользователь {user.name} (ID: {user.user_id}) добавлен.")
+        print(f"Пользователь {user.get_name()} (ID: {user.get_user_id()}) добавлен.")
 
     def remove_user(self, user_list: list, user_id: int):
 
         for user in user_list:
-            if user.user_id == user_id:
+            if user.get_user_id() == user_id:
                 user_list.remove(user)
-                print(f"Пользователь {user.name} (ID: {user_id}) удален.")
+                print(f"Пользователь {user.get_name()} (ID: {user_id}) удален.")
                 return
         print(f"Пользователь с ID {user_id} не найден.")
